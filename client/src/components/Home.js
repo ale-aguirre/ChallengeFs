57f6b569a51a9fbdabe3ac13dd88af9d9cec072a
ale-aguirre <ale.aguirre.2222@hotmail.com>
2021-02-25 17:51:17 -0300

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import ProductList from './ProductList'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <ProductList/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/components/ProductList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const ProductList = () => {
+    return (
+        //mapeo de las productcard
+        <div>
+            
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+
+            
+        </div>
+    )
+}
+
+export default ProductList
